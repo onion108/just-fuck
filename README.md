33dea4f95a4583080ee294fc46b31a88dcbde92d
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-08-13 10:06:46 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JustFuck
+
+A `JSFuck` encoder implemented in rust.
+
+[See The Original Project](https://github.com/aemkei/jsfuck)
+
+## Overview
+`JustFuck` is a simple `JSFuck` encoder implemented in `Rust`. For usages, you can run `justfuck --help` after installing the justfuck.
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,48 @@ use std::process::exit;
 use clap::Parser;
 
 use app::{user_friendly_read_from_file, user_friendly_write_to_file};
+use crossterm::style::Stylize;
+use justfuck::mapper::evaled_string;
 
-use crate::justfuck::mapper::encode_string;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+
     /// The input file.
+    #[clap(value_parser)]
     input_file: String,
+
     /// The output file. 
+    #[clap(long, value_parser, default_value="")]
     output_file: String,
+
 }
 
 mod justfuck;
 mod app;
 
 fn main() {
-    let content = match user_friendly_read_from_file("examples/test.js") {
+    let args = Args::parse();
+    let app_name = &std::env::args().collect::<Vec<String>>()[0];
+    let input_filename = args.input_file;
+    let output_filename: String;
+    if args.output_file == "" {
+        output_filename = String::from(String::from(input_filename.trim_end_matches(".js")) + ".fuck.js")
+    } else {
+        output_filename = args.output_file;
+    }
+    let original_content = match user_friendly_read_from_file(&input_filename)  {
         Ok(content) => content,
-        Err(why) => {
-            println!("{}", why);
+        Err(error) => {
+            eprintln!("{}", format!("{}:{}", app_name, error).bold().red());
             exit(-1);
-        },
+        }
     };
-    match user_friendly_write_to_file("examples/test.fuck.js", &encode_string(&content)) {
+    match user_friendly_write_to_file(&output_filename, &evaled_string(&original_content)) {
         Ok(()) => {}
-        Err(why) => {
-            println!("{}", why);
+        Err(error) => {
+            eprintln!("{}", format!("{}:{}", app_name, error).bold().red());
             exit(-1);
         }
     }
